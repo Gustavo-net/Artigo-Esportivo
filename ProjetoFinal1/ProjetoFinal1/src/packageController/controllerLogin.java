6e230ba477e31f1d2ae793f544e5e4ceb678fcbc
vsistema <vsistema@LAB5-01.sistemafieto.rede>
2024-10-15 16:50:47 -0300

    hm

--- ProjetoFinal1/ProjetoFinal1/src/packageController/controllerLogin.java
+++ ProjetoFinal1/ProjetoFinal1/src/packageController/controllerLogin.java
@@ -9,55 +9,53 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import packageModel.Funcionarios;
-import package_controle.FuncionarioDAO; // Importando a classe FuncionarioDAO
+import package_controle.FuncionarioDAO;
 
 public class controllerLogin {
 
-	@FXML
-	private TextField txtSenha;
-	@FXML
-	private Button btnLogar;
-	@FXML
-	private PasswordField txtPassword;
-	@FXML
-	private TextField txtUser; 
-	@FXML
-	private ToggleButton verSenha; 
-	static Funcionarios funcionario = new Funcionarios(); 
+    @FXML
+    private TextField txtSenha;
+    @FXML
+    private Button btnLogar;
+    @FXML
+    private PasswordField txtPassword;
+    @FXML
+    private TextField txtUser;
+    @FXML
+    private ToggleButton verSenha;
+    static Funcionarios funcionario = new Funcionarios();
 
+    @FXML
+    void btnActionLogar(ActionEvent event) {
+        FuncionarioDAO fDAO = new FuncionarioDAO();
+       
+        Funcionarios funcionarios = fDAO.autenticarUser(txtUser.getText(), txtPassword.getText());
 
-	@FXML
-	void btnActionLogar(ActionEvent event) {
-		FuncionarioDAO fDAO = new FuncionarioDAO(); 
-		
-		Funcionarios funcionarios = fDAO.autenticarUser(txtUser.getText(), txtPassword.getText());
-
-		if ( funcionarios.getCpf() != null && funcionarios.getSenha() != null) {
-			Alert saudacao = new Alert(Alert.AlertType.CONFIRMATION);
-			saudacao.setHeaderText("Saudação");
-			saudacao.setTitle("Bem-vindo");
-			saudacao.setContentText("Seja bem-vindo de volta, " + funcionarios.getNomeFuncionario() + "!");
-			saudacao.show();
-			Main.changeScreen("main");
-		} else {
-			Alert erro = new Alert(Alert.AlertType.ERROR);
-			erro.setTitle("Falha ao realizar o login");
-			erro.setHeaderText("Erro!");
-			erro.setContentText("Usuário ou senha incorretos! Verifique se as informações estão corretas.");
-			erro.show();
-		}
-	}
+        if (funcionarios != null && funcionarios.getCpf() != null && funcionarios.getSenha() != null) {
+            Alert saudacao = new Alert(Alert.AlertType.CONFIRMATION);
+            saudacao.setHeaderText("Saudação");
+            saudacao.setTitle("Bem-vindo");
+            saudacao.setContentText("Seja bem-vindo de volta, " + funcionarios.getNomeFuncionario() + "!");
+            saudacao.show();
+            Main.changeScreen("main");
+        } else {
+            Alert erro = new Alert(Alert.AlertType.ERROR);
+            erro.setTitle("Falha ao realizar o login");
+            erro.setHeaderText("Erro!");
+            erro.setContentText("Usuário ou senha incorretos! Verifique se as informações estão corretas.");
+            erro.show();
+        }
+    }
 
     @FXML
     void OnVerSenha(ActionEvent event) {
-    	if (verSenha.isSelected()) {
-    		txtSenha.setText(txtPassword.getText());
-			txtPassword.setVisible(false);
-			txtSenha.setText(txtPassword.getText()); 
-		} else {
-			txtPassword.setVisible(true);
-			txtUser.setText(""); 
-		}
-	}
-    
+        if (verSenha.isSelected()) {
+            txtSenha.setText(txtPassword.getText());
+            txtPassword.setVisible(false);
+            txtSenha.setText(txtPassword.getText());
+        } else {
+            txtPassword.setVisible(true);
+            txtUser.setText("");
+        }
+    }
 }
--- ProjetoFinal1/ProjetoFinal1/src/package_controle/FuncionarioDAO.java
+++ ProjetoFinal1/ProjetoFinal1/src/package_controle/FuncionarioDAO.java
@@ -11,189 +11,186 @@ import packageModel.Funcionarios;
 
 public class FuncionarioDAO {
 
-	public void create(Funcionarios c) {
-		Connection con = ConnectionDatabase.getConnection();
-		PreparedStatement stmt = null;
+    public void create(Funcionarios c) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
 
-		try {
-			stmt = con.prepareStatement(
-					"INSERT INTO Funcionario (idFuncionario, nomeFuncionario, cpf, email, telefone, dataNasc, dataCont, cargo, sexo, senha, id_Endereco) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-			stmt.setString(1, c.getIdFuncionario());
-			stmt.setString(2, c.getNomeFuncionario());
-			stmt.setString(3, c.getCpf());
-			stmt.setString(4, c.getEmail());
-			stmt.setString(5, c.getTelefone());
-			stmt.setString(6, c.getDataNasc());
-			stmt.setString(7, c.getDataCont());
-			stmt.setString(8, c.getCargo());
-			stmt.setString(9, c.getSexo());
-			stmt.setString(10, c.getSenha());
-			stmt.setString(11, c.getId_Endereço());
+        try {
+            stmt = con.prepareStatement(
+                    "INSERT INTO Funcionarios (idFuncionario, nomeFuncionario, cpf, email, telefone, dataNasc, dataCont, cargo, sexo, senha, id_Endereco) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            stmt.setString(1, c.getIdFuncionario());
+            stmt.setString(2, c.getNomeFuncionario());
+            stmt.setString(3, c.getCpf());
+            stmt.setString(4, c.getEmail());
+            stmt.setString(5, c.getTelefone());
+            stmt.setString(6, c.getDataNasc());
+            stmt.setString(7, c.getDataCont());
+            stmt.setString(8, c.getCargo());
+            stmt.setString(9, c.getSexo());
+            stmt.setString(10, c.getSenha());
+            stmt.setString(11, c.getId_Endereço());
 
-			stmt.executeUpdate();
+            stmt.executeUpdate();
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			ConnectionDatabase.closeConnection(con, stmt);
-		}
-	}
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt);
+        }
+    }
 
-	public ArrayList<Funcionarios> read() {
-		Connection con = ConnectionDatabase.getConnection();
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		ArrayList<Funcionarios> funcionario = new ArrayList<>();
+    public ArrayList<Funcionarios> read() {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ArrayList<Funcionarios> funcionarios = new ArrayList<>();
 
-		try {
-			stmt = con.prepareStatement("SELECT * FROM Funcionarios");
-			rs = stmt.executeQuery();
+        try {
+            stmt = con.prepareStatement("SELECT * FROM Funcionarios");
+            rs = stmt.executeQuery();
 
-			while (rs.next()) {
-				Funcionarios f = new Funcionarios();
-				f.setIdFuncionario(rs.getString("idCliente"));
-				f.setNomeFuncionario(rs.getString("nome"));
-				f.setCpf(rs.getString("cpf"));
-				f.setEmail(rs.getString("email"));
-				f.setTelefone(rs.getString("telefone"));
-				f.setDataNasc(rs.getString("dataNasc"));
-				f.setDataCont(rs.getString("dataCont"));
-				f.setCargo(rs.getString("cargo"));
-				f.setSexo(rs.getString("sexo"));
-				f.setSenha(rs.getString("senha"));
-				f.setId_Endereço(rs.getString("idEndereco"));
-				funcionario.add(f);
-			}
+            while (rs.next()) {
+                Funcionarios f = new Funcionarios();
+                f.setIdFuncionario(rs.getString("idFuncionario"));
+                f.setNomeFuncionario(rs.getString("nomeFuncionario"));
+                f.setCpf(rs.getString("cpf"));
+                f.setEmail(rs.getString("email"));
+                f.setTelefone(rs.getString("telefone"));
+                f.setDataNasc(rs.getString("dataNasc"));
+                f.setDataCont(rs.getString("dataCont"));
+                f.setCargo(rs.getString("cargo"));
+                f.setSexo(rs.getString("sexo"));
+                f.setSenha(rs.getString("senha"));
+                f.setId_Endereço(rs.getString("id_Endereco"));
+                funcionarios.add(f);
+            }
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			ConnectionDatabase.closeConnection(con, stmt, rs);
-		}
-		return funcionario;
-	}
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt, rs);
+        }
+        return funcionarios;
+    }
 
-	public ArrayList<Funcionarios> search(String string) {
-		Connection con = ConnectionDatabase.getConnection();
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		ArrayList<Funcionarios> funcionario = new ArrayList<>();
+    public ArrayList<Funcionarios> search(String string) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ArrayList<Funcionarios> funcionarios = new ArrayList<>();
 
-		try {
-			stmt = con.prepareStatement("SELECT * FROM Funcionarios WHERE nomeFuncionario LIKE ?");
-			stmt.setString(1, "%" + string + "%");
-			rs = stmt.executeQuery();
+        try {
+            stmt = con.prepareStatement("SELECT * FROM Funcionarios WHERE nomeFuncionario LIKE ?");
+            stmt.setString(1, "%" + string + "%");
+            rs = stmt.executeQuery();
 
-			while (rs.next()) {
-				Funcionarios f = new Funcionarios();
-				f.setIdFuncionario(rs.getString("idFuncionario"));
-				f.setNomeFuncionario(rs.getString("nomeFuncionario"));
-				f.setCpf(rs.getString("cpf"));
-				f.setEmail(rs.getString("email"));
-				f.setTelefone(rs.getString("telefone"));
-				f.setDataNasc(rs.getString("dataNasc"));
-				f.setDataCont(rs.getString("dataPrimComp"));
-				f.setCargo(rs.getString("endereco"));
-				f.setSenha(rs.getString("senha"));
-				f.setCargo(rs.getString("endereco"));
-				f.setSexo(rs.getString("sexo"));
-				f.setSenha(rs.getString("senha"));
-				f.setId_Endereço(rs.getString("id_Endereco"));
-				funcionario.add(f);
-			}
+            while (rs.next()) {
+                Funcionarios f = new Funcionarios();
+                f.setIdFuncionario(rs.getString("idFuncionario"));
+                f.setNomeFuncionario(rs.getString("nomeFuncionario"));
+                f.setCpf(rs.getString("cpf"));
+                f.setEmail(rs.getString("email"));
+                f.setTelefone(rs.getString("telefone"));
+                f.setDataNasc(rs.getString("dataNasc"));
+                f.setDataCont(rs.getString("dataCont"));
+                f.setCargo(rs.getString("cargo"));
+                f.setSexo(rs.getString("sexo"));
+                f.setSenha(rs.getString("senha"));
+                f.setId_Endereço(rs.getString("id_Endereco"));
+                funcionarios.add(f);
+            }
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			ConnectionDatabase.closeConnection(con, stmt, rs);
-		}
-		return funcionario;
-	}
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt, rs);
+        }
+        return funcionarios;
+    }
 
-	public void update(Funcionarios c) {
-		Connection con = ConnectionDatabase.getConnection();
-		PreparedStatement stmt = null;
+    public void update(Funcionarios c) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
 
-		try {
-			stmt = con.prepareStatement(
-					"UPDATE Funcionarios SET nomeFuncionario = ?, cpf = ?, email = ?, telefone = ?, dataNasc = ?, dataCont = ?, cargo = ?, sexo = ?, senha = ?, id_Endereco WHERE idFuncionario = ?");
-			stmt.setString(1, c.getNomeFuncionario());
-			stmt.setString(2, c.getCpf());
-			stmt.setString(3, c.getEmail());
-			stmt.setString(4, c.getTelefone());
-			stmt.setString(5, c.getDataNasc());
-			stmt.setString(6, c.getDataCont());
-			stmt.setString(7, c.getCargo());
-			stmt.setString(8, c.getSexo());
-			stmt.setString(9, c.getSenha());
-			stmt.setString(10, c.getId_Endereço());
-			stmt.setString(11, c.getIdFuncionario());
+        try {
+            stmt = con.prepareStatement(
+                    "UPDATE Funcionarios SET nomeFuncionario = ?, cpf = ?, email = ?, telefone = ?, dataNasc = ?, dataCont = ?, cargo = ?, sexo = ?, senha = ?, id_Endereco = ? WHERE idFuncionario = ?");
+            stmt.setString(1, c.getNomeFuncionario());
+            stmt.setString(2, c.getCpf());
+            stmt.setString(3, c.getEmail());
+            stmt.setString(4, c.getTelefone());
+            stmt.setString(5, c.getDataNasc());
+            stmt.setString(6, c.getDataCont());
+            stmt.setString(7, c.getCargo());
+            stmt.setString(8, c.getSexo());
+            stmt.setString(9, c.getSenha());
+            stmt.setString(10, c.getId_Endereço());
+            stmt.setString(11, c.getIdFuncionario());
 
-			stmt.executeUpdate();
+            stmt.executeUpdate();
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			ConnectionDatabase.closeConnection(con, stmt);
-		}
-	}
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt);
+        }
+    }
 
-	public void delete(String idFuncionario) {
+    public void delete(String idFuncionario) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
 
-		Connection con = ConnectionDatabase.getConnection();
-		PreparedStatement stmt = null;
+        try {
+            stmt = con.prepareStatement("DELETE FROM Funcionarios WHERE idFuncionario = ?");
+            stmt.setString(1, idFuncionario);
 
-		try {
-			stmt = con.prepareStatement("DELETE FROM Funcionarios WHERE idfuncionario = ?");
-			stmt.setString(1, idFuncionario);
+            stmt.executeUpdate();
 
-			stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Erro ao excluir o funcionário: " + e.getMessage(), e);
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new RuntimeException("Erro ao excluir o funcionario: " + e.getMessage(), e);
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt);
+        }
+    }
 
-		} finally {
-			ConnectionDatabase.closeConnection(con, stmt);
-		}
-	}
+    public Funcionarios autenticarUser(String cpf, String senha) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Funcionarios funcionarios = null;
 
-	public Funcionarios autenticarUser(String cpf, String senha) {
-		Connection con = ConnectionDatabase.getConnection();
-		PreparedStatement stmt = null;
-		ResultSet rs = null;
-		Funcionarios funcionarios = null;
+        try {
+            String sql = "SELECT * FROM Funcionarios WHERE cpf = ? AND senha = ?";
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, cpf);
+            stmt.setString(2, senha);
+           
+            rs = stmt.executeQuery();
 
-		try {
-			String sql = "SELECT * FROM Vendedor WHERE senha = ? AND (cpf = ?)";
-			stmt = con.prepareStatement(sql);
-			stmt.setString(1, cpf);
-			stmt.setString(2, senha);
+            if (rs.next()) {
+                funcionarios = new Funcionarios();
+                funcionarios.setIdFuncionario(rs.getString("idFuncionario"));
+                funcionarios.setNomeFuncionario(rs.getString("nomeFuncionario"));
+                funcionarios.setCpf(rs.getString("cpf"));
+                funcionarios.setEmail(rs.getString("email"));
+                funcionarios.setTelefone(rs.getString("telefone"));
+                funcionarios.setDataNasc(rs.getString("dataNasc"));
+                funcionarios.setDataCont(rs.getString("dataCont"));
+                funcionarios.setCargo(rs.getString("cargo"));
+                funcionarios.setSexo(rs.getString("sexo"));
+                funcionarios.setSenha(rs.getString("senha"));
+                funcionarios.setId_Endereço(rs.getString("id_Endereco"));
+            }
 
-			rs = stmt.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt, rs);
+        }
 
-			if (rs.next()) {
-				funcionarios = new Funcionarios();
-				funcionarios.setIdFuncionario(rs.getString(1));
-				funcionarios.setNomeFuncionario(rs.getString(2));
-				funcionarios.setCpf(rs.getString(3));
-				funcionarios.setEmail(rs.getString(4));
-				funcionarios.setTelefone(rs.getString(5));
-				funcionarios.setDataNasc(rs.getString(6));
-				funcionarios.setDataCont(rs.getString(7));
-				funcionarios.setCargo(rs.getString(8));
-				funcionarios.setSexo(rs.getString(9));
-				funcionarios.setSenha(rs.getString(10));
-				funcionarios.setId_Endereço(rs.getString(11));
-			}
+        return funcionarios;
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			ConnectionDatabase.closeConnection(con, stmt, rs);
-		}
-
-		return funcionarios;
-
-	}
+    }
 }
