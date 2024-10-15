ad49d0202f32a598533902ea161320b4552dc074
vsistema <vsistema@LAB5-13.sistemafieto.rede>
2024-10-15 15:16:15 -0300

    teste

--- ProjetoFinal1/ProjetoFinal1/src/packageController/controllerRelatorioProduto.java
+++ ProjetoFinal1/ProjetoFinal1/src/packageController/controllerRelatorioProduto.java
@@ -1,110 +1,83 @@
 package packageController;
 
-
-
-import application.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import packageModel.Produtos;
 
 public class controllerRelatorioProduto {
-
 	@FXML
 	private TableView<Produtos> ViewProdutos;
-
+	@FXML
+	private ComboBox<Produtos> boxFiltrar;
 	@FXML
 	private Button btnCadastros;
-
 	@FXML
 	private Button btnClientes;
-
 	@FXML
 	private Button btnEditar;
-
-	@FXML
-	private Button btnExcluir;
-
 	@FXML
 	private Button btnFornecedores;
-
 	@FXML
 	private Button btnFuncionários;
-
+	@FXML
+	private Button btnInserir;
 	@FXML
 	private Button btnPesquisar;
-
-	@FXML
-	private Button btnSalvar;
-
 	@FXML
 	private Button btnVendas;
-
+	@FXML
+	private Button btnVoltar;
 	@FXML
 	private TableColumn<Produtos, String> columnCodigo;
-
 	@FXML
-	private TableColumn<Produtos, String> columnDescrição;
-
+	private TableColumn<Produtos, String> columnDescricao;
 	@FXML
 	private TableColumn<Produtos, String> columnEstoqueAtual;
-
+	@FXML
+	private TableColumn<Produtos, String> columnId;
 	@FXML
 	private TableColumn<Produtos, String> columnMarca;
-
 	@FXML
 	private TableColumn<Produtos, String> columnNome;
-
 	@FXML
-	private TableColumn<Produtos, String> columnPreçoUn;
-
+	private TableColumn<Produtos, String> columnPrecoUn;
 	@FXML
-	private TableColumn<Produtos, String> columnidProduto;
+	private TextField txtPesquisar;
 
 	@FXML
 	void OnbtnCadastros(ActionEvent event) {
-		Main.changeScreen("cadastros");
+	}
 
+	@FXML
+	void OnbtnEditar(ActionEvent event) {
 	}
 
 	@FXML
 	void OnbtnFornecedores(ActionEvent event) {
-		Main.changeScreen("fornecedores");
-
 	}
 
 	@FXML
 	void OnbtnFuncionários(ActionEvent event) {
-		Main.changeScreen("funcionarios");
-
 	}
 
 	@FXML
-	void OnbtnVendas(ActionEvent event) {
-		Main.changeScreen("vendas");
-
+	void OnbtnInserir(ActionEvent event) {
 	}
 
 	@FXML
 	void OnbtnPesquisar(ActionEvent event) {
-
 	}
 
 	@FXML
-	void btnActionEditar(ActionEvent event) {
-
+	void OnbtnVendas(ActionEvent event) {
 	}
 
 	@FXML
-	void btnActionExcluir(ActionEvent event) {
-
+	void OnbtnVoltar(ActionEvent event) {
 	}
-
-	@FXML
-	void btnActionSalvar(ActionEvent event) {
-
-	}
-
 }
