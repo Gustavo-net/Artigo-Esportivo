5b70c3f5d401ee9db4aa4eb38755862ff6953cf8
vsistema <vsistema@LAB5-13.sistemafieto.rede>
2024-10-21 15:10:53 -0300

    commit conect

--- ProjetoFinal1/ProjetoFinal1/src/packageConnection/ConnectionDatabase.java
+++ ProjetoFinal1/ProjetoFinal1/src/packageConnection/ConnectionDatabase.java
@@ -12,6 +12,7 @@ public class ConnectionDatabase {
     private final static String URL = "jdbc:sqlserver://192.168.50.189:57350;encrypt=false;databaseName=Artigos_Esportivos";
     private final static String USER = "SA";
     private final static String PASSWORD = "Senailab05";
+    
 
     public static Connection getConnection() {
         try {
