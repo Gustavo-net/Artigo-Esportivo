4eaf2bcf5369a161c8bf28976ae9134a9dc80396
vsistema <vsistema@LAB5-01.sistemafieto.rede>
2024-10-22 14:42:23 -0300

    CARLOS GAYYYYYYYYYYY

---
+++ ProjetoFinal1/ProjetoFinal1/src/packageController/controllerCadastrosFornecedor.java
@@ -0,0 +1,48 @@
+package packageController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class controllerCadastrosFornecedor {
+
+    @FXML
+    private Button btnAddProduto;
+
+    @FXML
+    private TextField txtBairro;
+
+    @FXML
+    private TextField txtCNPJ;
+
+    @FXML
+    private TextField txtCep;
+
+    @FXML
+    private TextField txtCidadeUF;
+
+    @FXML
+    private TextField txtComplemento;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtNomeProduto;
+
+    @FXML
+    private TextField txtNumero;
+
+    @FXML
+    private TextField txtRua;
+
+    @FXML
+    private TextField txtTelefone;
+
+    @FXML
+    void OnbtnAddProduto(ActionEvent event) {
+
+    }
+
+}
---
+++ ProjetoFinal1/ProjetoFinal1/src/packageController/controllerCadastrosProdutos.java
@@ -0,0 +1,52 @@
+package packageController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class controllerCadastrosProdutos {
+
+    @FXML
+    private ComboBox<?> boxCategoria;
+
+    @FXML
+    private Button btnAddProduto;
+
+    @FXML
+    private TextField txtCodigo;
+
+    @FXML
+    private TextField txtCor;
+
+    @FXML
+    private TextField txtDescricao;
+
+    @FXML
+    private TextField txtEstoqueAtual;
+
+    @FXML
+    private TextField txtEstoqueMinimo;
+
+    @FXML
+    private TextField txtMarca;
+
+    @FXML
+    private TextField txtNomeProduto;
+
+    @FXML
+    private TextField txtPreco;
+
+    @FXML
+    private TextField txtTamanho;
+
+    @FXML
+    private TextField txtTamanhoMaximo;
+
+    @FXML
+    void OnbtnAddProduto(ActionEvent event) {
+
+    }
+
+}
