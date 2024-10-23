6f4fe0092048d238b9bf9541549947b60facbdee
vsistema <vsistema@LAB5-01.sistemafieto.rede>
2024-10-23 13:52:40 -0300

    Controller Cliente

---
+++ ProjetoFinal1/ProjetoFinal1/src/packageController/controllerCadastrosClientes.java
@@ -0,0 +1,56 @@
+package packageController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class  controllerCadastrosClientes  {
+
+    @FXML
+    private Button btnAddCliente;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private TextField txtBairro;
+
+    @FXML
+    private TextField txtCPF;
+
+    @FXML
+    private TextField txtCep;
+
+    @FXML
+    private TextField txtCidadeUF;
+
+    @FXML
+    private TextField txtComplemento;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtNomeCliente;
+
+    @FXML
+    private TextField txtNumero;
+
+    @FXML
+    private TextField txtRua;
+
+    @FXML
+    private TextField txtTelefone;
+
+    @FXML
+    void OnbtnAddCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnbtnCancelar(ActionEvent event) {
+
+    }
+
+}
