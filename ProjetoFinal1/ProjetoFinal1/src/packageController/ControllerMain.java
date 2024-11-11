de006f87618ca86dda7d4acc31017f4f046d57f4
vsistema <vsistema@LAB5-13.sistemafieto.rede>
2024-11-11 17:26:32 -0300

    dss

--- ProjetoFinal1/ProjetoFinal1/src/packageController/ControllerMain.java
+++ ProjetoFinal1/ProjetoFinal1/src/packageController/ControllerMain.java
@@ -1,4 +1,4 @@
-package packageController; 
+package packageController;
 
 import application.Main;
 import javafx.beans.property.SimpleStringProperty;
@@ -6,13 +6,20 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import packageModel.Funcionarios;
 import packageModel.ProdutoBaixoEstoque;
+import packageModel.Venda;
+import package_controle.FuncionarioDAO;
+import package_controle.VendasDAO;
 import package_controle.baixoEstoqueDAO;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ControllerMain {
 
@@ -44,18 +51,74 @@ public class ControllerMain {
     private TableColumn<ProdutoBaixoEstoque, String> columnQuantidade;
 
     @FXML
+    private Label labelFaturamento;
+    @FXML
+    private Label labelFuncionarios;
+    @FXML
+    private Label labelVendas;
+
+    private VendasDAO vendasDAO;
+    private FuncionarioDAO funcionarioDAO;
+    private baixoEstoqueDAO baixoEstoqueDAO;
+
+    public ControllerMain() {
+        this.vendasDAO = new VendasDAO();
+        this.funcionarioDAO = new FuncionarioDAO();
+        this.baixoEstoqueDAO = new baixoEstoqueDAO();
+    }
+
+    @FXML
     void initialize() {
-    	 columnNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
-    	    columnCodigo.setCellValueFactory(new PropertyValueFactory<>("codigo"));
-    	    columnMarca.setCellValueFactory(new PropertyValueFactory<>("marca"));
-    	    columnCategoria.setCellValueFactory(new PropertyValueFactory<>("categoria"));
-    	    columnQuantidade.setCellValueFactory(new PropertyValueFactory<>("quantidadeEstoque"));
+        columnNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        columnCodigo.setCellValueFactory(new PropertyValueFactory<>("codigo"));
+        columnMarca.setCellValueFactory(new PropertyValueFactory<>("marca"));
+        columnCategoria.setCellValueFactory(new PropertyValueFactory<>("categoria"));
+        columnQuantidade.setCellValueFactory(new PropertyValueFactory<>("quantidadeEstoque"));
+
         carregarProdutos();
+        exibirRelatorio();  
     }
 
     private void carregarProdutos() {
-        ArrayList<ProdutoBaixoEstoque> produtosBaixoEstoque = baixoEstoqueDAO.read(); 
+        ArrayList<ProdutoBaixoEstoque> produtosBaixoEstoque = baixoEstoqueDAO.read();
         ViewProdutos.setItems(FXCollections.observableArrayList(produtosBaixoEstoque));
+    }
+
+    private void exibirRelatorio() {
+        try {
+            double faturamentoTotal = calcularFaturamentoTotal();
+            int numeroFuncionarios = contarFuncionarios();
+            int numeroVendas = contarVendasRealizadas();
+
+            labelFaturamento.setText(String.format("R$ %.2f", faturamentoTotal));
+            labelFuncionarios.setText(""+numeroFuncionarios);
+            labelVendas.setText(""+ numeroVendas);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public double calcularFaturamentoTotal() throws SQLException {
+        double faturamentoTotal = 0.0;
+
+        List<Venda> vendas = vendasDAO.listarVendas();
+
+        for (Venda venda : vendas) {
+            faturamentoTotal += venda.getTotalVenda();
+        }
+
+        return faturamentoTotal;
+    }
+
+    public int contarFuncionarios() throws SQLException {
+        List<Funcionarios> funcionarios = funcionarioDAO.read();
+        return funcionarios.size();
+    }
+
+    public int contarVendasRealizadas() throws SQLException {
+        List<Venda> vendas = vendasDAO.listarVendas();
+        return vendas.size();
     }
 
     @FXML
@@ -67,7 +130,7 @@ public class ControllerMain {
     void OnbtnFornecedores(ActionEvent event) {
         Main.changeScreen("fornecedores");
     }
-    
+
     @FXML
     void OnbtnFuncionários(ActionEvent event) {
         Main.changeScreen("funcionarios");
